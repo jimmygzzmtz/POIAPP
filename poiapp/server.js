e414e5220cdb271b215afa567c89bd50fdc40fa0
Jaime González <jimmygzzmtz@gmail.com>
2019-05-10 15:35:31 -0500

    .

--- poiapp/server.js
+++ poiapp/server.js
@@ -2,13 +2,11 @@ var express = require('express');
 var path = require('path')
 var app = express();
 
-const router = express.Router()
-router.get('/home', function (req, res) {
+app.use(express.static(path.resolve(__dirname, "www")));
+
+app.get('/home', function (req, res) {
     res.send('root')
 })
-app.use(router)
-
-app.use(express.static(path.resolve(__dirname, "www")));
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
